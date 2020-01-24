4c805a523b9fb7b5e70226333a17cc059943ac2f
Bram Buurlage <brambuurlage@gmail.com>
2020-01-24 11:13:03 +0100

    Raycast working like it should

--- src/raycast.rs
+++ src/raycast.rs
@@ -2,6 +2,7 @@ use crate::triangulate::Triangulate;
 use crate::voxel::{Data, Voxel};
 use crate::world::VoxelWorld;
 use nalgebra_glm::*;
+//use nalgebra_glm::convert
 use std::any::Any;
 use std::ops::{Deref, DerefMut};
 
@@ -121,14 +122,18 @@ impl<V: Data> Raycast for VoxelWorld<V> {
     type Child = Voxel<V>;
 
     fn cast(&self, ray: &Ray) -> Option<Intersection> {
+        let origin = vec3(
+            self.origin[0] as f32,
+            self.origin[1] as f32,
+            self.origin[2] as f32,
+        );
         // the current location being checked on the ray
-        let current = ray.origin * (1.0 / self.scale)
-            - vec3(
-                self.origin[0] as f32,
-                self.origin[1] as f32,
-                self.origin[2] as f32,
-            );
-        cast(self, ray, current, ray.direction, 30)
+        let current = ray.origin * (1.0 / self.scale) - origin;
+        cast(self, ray, current, ray.direction, 30).map(|mut intersection| {
+            intersection.position = intersection.position + origin;
+            intersection.position = intersection.position * self.scale;
+            intersection
+        })
     }
 
     fn check(&self, ray: &Ray, current: Vec3, coord: [isize; 3]) -> Option<Intersection> {
@@ -153,20 +158,23 @@ impl<V: Data> Raycast for VoxelWorld<V> {
                         direction: ray.direction,
                         length: ray.length,
                     };
-                    let current = current * (1.0 / sc);
-                    let intersection =
-                        ray.transform * vec4(current[0], current[1], current[2], 1.0);
-                    let intersection = vec4_to_vec3(&intersection);
                     if let Some(sub) = voxel.cast(&r) {
                         return Some(Intersection {
                             inner: Some(Box::new(sub)),
                             index: i,
-                            position: intersection,
+                            position: current,
                         });
                     }
                 }
+            } else {
+                return Some(Intersection {
+                    inner: None,
+                    index: 0,
+                    position: current,
+                });
             }
         }
+        
         None
     }
 
@@ -208,7 +216,13 @@ impl<T: Data> Raycast for Voxel<T> {
             }
         }
 
-        cast(self, ray, current, current_direction, 6 * Self::WIDTH)
+        cast(self, ray, current, current_direction, 6 * Self::WIDTH).map(|mut intersection| {
+            let mut pos = vec3_to_vec4(&intersection.position) / scale;
+            pos.w = 1.0;
+            pos = ray.transform * pos;
+            intersection.position = vec4_to_vec3(&pos);
+            intersection
+        })
     }
 
     fn check(&self, ray: &Ray, current: Vec3, coord: [isize; 3]) -> Option<Intersection> {
@@ -218,39 +232,34 @@ impl<T: Data> Raycast for Voxel<T> {
             let i = coord[0] as usize + coord[1] as usize * Self::DY + coord[2] as usize * Self::DZ;
             if let Some(voxel) = self.get(i) {
                 if voxel.visible() {
-                    let sc = Self::SCALE;
-                    let s = scaling(&vec3(sc, sc, sc));
-                    let t = translation(&vec3(
-                        coord[0] as f32 * sc,
-                        coord[1] as f32 * sc,
-                        coord[2] as f32 * sc,
-                    ));
-                    let r = Ray {
-                        transform: ray.transform * t * s,
-                        origin: ray.origin,
-                        direction: ray.direction,
-                        length: ray.length,
-                    };
-                    let current = current * (1.0 / sc);
-                    let intersection =
-                        ray.transform * vec4(current[0], current[1], current[2], 1.0);
-                    let intersection = vec4_to_vec3(&intersection);
-
                     match voxel {
                         Voxel::Empty { .. } => (),
                         Voxel::Material { .. } => {
                             return Some(Intersection {
                                 inner: None,
                                 index: i,
-                                position: intersection,
+                                position: current,
                             });
                         }
                         Voxel::Detail { .. } => {
+                            let sc = Self::SCALE;
+                            let s = scaling(&vec3(sc, sc, sc));
+                            let t = translation(&vec3(
+                                coord[0] as f32 * sc,
+                                coord[1] as f32 * sc,
+                                coord[2] as f32 * sc,
+                            ));
+                            let r = Ray {
+                                transform: ray.transform * t * s,
+                                origin: ray.origin,
+                                direction: ray.direction,
+                                length: ray.length,
+                            };
                             if let Some(sub) = voxel.cast(&r) {
                                 return Some(Intersection {
                                     inner: Some(Box::new(sub)),
                                     index: i,
-                                    position: intersection,
+                                    position: current,
                                 });
                             }
                         }
@@ -284,12 +293,12 @@ fn cast<R: Raycast>(
     // keep the current location as integer coordinates, to mitigate rounding errors on
     //  integrated values
     let mut current_i = [
-        current[0] as isize,
-        current[1] as isize,
-        current[2] as isize,
+        current[0].floor() as isize,
+        current[1].floor() as isize,
+        current[2].floor() as isize,
     ];
     for i in 0..3 {
-        if (current[i].floor() - current[i]).abs() < std::f32::EPSILON && direction[i] < 0.0 {
+        if current[i] - current[i].floor() < std::f32::EPSILON && direction[i] < 0.0 {
             current_i[i] -= 1;
         }
     }
@@ -303,9 +312,9 @@ fn cast<R: Raycast>(
     for _ in 0..iterations {
         // try to find the nearest intersection with the grid
         let i = vec3(
-            intersect(current[0], direction[0]),
-            intersect(current[1], direction[1]),
-            intersect(current[2], direction[2]),
+            intersect(current_i[0], current[0], direction[0]),
+            intersect(current_i[1], current[1], direction[1]),
+            intersect(current_i[2], current[2], direction[2]),
         );
 
         // advance by the distance to the nearest grid intersection
@@ -316,7 +325,7 @@ fn cast<R: Raycast>(
                 current += direction * i[d];
                 if direction[d] < 0.0 {
                     current_i[d] -= 1;
-                    current[d] = current_i[d] as f32;
+                    current[d] = current_i[d] as f32 + 1.0;
                     if let Some(hit) = raycast.check(ray, current, current_i) {
                         return Some(hit);
                     }
@@ -335,26 +344,15 @@ fn cast<R: Raycast>(
 }
 
 /// find nearest intersection with a 1d grid, with grid lines at all integer positions
-fn intersect(position: f32, direction: f32) -> f32 {
+fn intersect(reference: isize, position: f32, direction: f32) -> f32 {
     if direction == 0.0 {
         ::std::f32::INFINITY
     } else {
         let target = if direction < 0.0 {
-            let t = position.floor();
-            if (t - position).abs() < std::f32::EPSILON {
-                t - 1.0
-            } else {
-                t
-            }
+            reference as f32
         } else {
-            let t = position.ceil();
-            if (t - position).abs() < std::f32::EPSILON {
-                t + 1.0
-            } else {
-                t
-            }
+            (reference + 1) as f32
         };
-
         (target - position) / direction
     }
 }
